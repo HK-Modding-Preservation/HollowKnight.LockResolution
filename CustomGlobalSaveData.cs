57a3eb8bac5d47a45f3d7f62ecc0e10f5960108c
wdghzym <67120670+wdghzym@users.noreply.github.com>
2024-04-17 12:12:19 +0800

    mod menu

---
+++ CustomGlobalSaveData.cs
@@ -0,0 +1,10 @@
+﻿namespace LockResolution
+{
+    public class CustomGlobalSaveData
+    {
+        public bool locked = true;
+        public int width = UnityEngine.Screen.width;
+        public int height = UnityEngine.Screen.height;
+        public bool fullScreen = UnityEngine.Screen.fullScreen;
+    }
+}
--- LockResolution.cs
+++ LockResolution.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using Modding;
 
 namespace LockResolution
 {
-    public class LockResolution:Mod
+    public class LockResolution : Mod, IGlobalSettings<CustomGlobalSaveData>, IMenuMod
     {
+        public LockResolution Instance;
+        public static CustomGlobalSaveData globalSaveData = new CustomGlobalSaveData();
+
+        public bool ToggleButtonInsideMenu => true;
 
         public override string GetVersion()
         {
@@ -21,14 +26,66 @@ namespace LockResolution
         public override int LoadPriority() => 5;
         public override void Initialize()
         {
+            Instance = this;
             ModHooks.HeroUpdateHook += ModHooks_HeroUpdateHook;
+            //On.HutongGames.PlayMaker.Actions.ScreenSetResolution.OnEnter += ScreenSetResolution_OnEnter;
+            //On.HutongGames.PlayMaker.Actions.ScreenSetResolution.Reset += ScreenSetResolution_Reset;
+            //On.GameManager.Update
+            //MenuChanger.MenuChangerMod.
         }
         private void ModHooks_HeroUpdateHook()
         {
-            if (UnityEngine.Screen.width != 1920)
+            if (!globalSaveData.locked) return;
+            if (UnityEngine.Screen.width != globalSaveData.width)
             {
-                UnityEngine.Screen.SetResolution(1920, 1080, fullscreen: true);
+                UnityEngine.Screen.SetResolution(globalSaveData.width, globalSaveData.height, globalSaveData.fullScreen);
             }
         }
+        void IGlobalSettings<CustomGlobalSaveData>.OnLoadGlobal(CustomGlobalSaveData data)
+        {
+            globalSaveData = data;
+        }
+
+        CustomGlobalSaveData IGlobalSettings<CustomGlobalSaveData>.OnSaveGlobal()
+        {
+            return globalSaveData;
+        }
+        IMenuMod.MenuEntry menuEntry;
+        public List<IMenuMod.MenuEntry> GetMenuData(IMenuMod.MenuEntry? toggleButtonEntry)
+        {
+            menuEntry = new IMenuMod.MenuEntry
+            {
+                Name = "Lock",
+                Description = "Lock Resolution On GamePlay",
+                Values = new string[] { "True", "False" },
+                Saver = locked =>
+                {
+                    globalSaveData.locked = locked == 0;
+                    if (globalSaveData.locked)
+                    {
+                        globalSaveData.width = UnityEngine.Screen.width;
+                        globalSaveData.height = UnityEngine.Screen.height;
+                        globalSaveData.fullScreen = UnityEngine.Screen.fullScreen;
+                    }
+                    Log($"Saver {globalSaveData.locked} Width{globalSaveData.width} Height{globalSaveData.height}");
+                },
+                Loader = () =>
+                {
+                    Log($"Loader {globalSaveData.locked} Width{globalSaveData.width} Height{globalSaveData.height}");
+                    return globalSaveData.locked == true ? 0 : 1;
+                }
+            };
+            return new() { menuEntry };
+        }
+        /*
+        private void updateMenuName()
+        {
+            menuEntry.Name = globalSaveData.locked
+                ? $"Locked Width{globalSaveData.width} Height{globalSaveData.height}"
+                : $"Lock Current Width{UnityEngine.Screen.width} Height{UnityEngine.Screen.height}";
+
+            Log($"updateMenuName {globalSaveData.locked}");
+            Log($"updateMenuName {menuEntry.Name}");
+        }*/
     }
 }
